b706115eda2e06d455e3c21ead3a49884b852437
Haroliao <83564380+Haroliao@users.noreply.github.com>
2022-10-10 11:43:33 +1030

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,22 +6,29 @@
 
 using namespace std;
 int main(){
+    int temp;
     vector<int> input;
+    vector<int> sortedInput;
    QuickSort B;
    Sort * sortB = &B;
    //input=sortB->sort({-5,1,3,5,4,100,7777,2014,0});
-   input=sortB->sort({5,3,-5,4,100,7777,2014,0});
+   //input=sortB->sort({-5,-8,-4,-2,1});
+      for (int i=0;i<8;i++){
+        cin >> temp;
+        input.push_back(temp);
+    }
+    sortedInput=sortB->sort(input);
     //BubbleSort B;
     //Sort*sortB=&B;
   //input=sortB->sort({4,3,2,6});
    RecursiveBinarySearch A;
   bool ans;
-  ans=A.search(input,1);
+  ans=A.search(sortedInput,1);
   if (ans==0){cout <<"false ";}
  if (ans==1){cout <<"true ";}
-     int size=input.size();
+     int size=sortedInput.size();
     for (int i=0;i<size;i++){
-        cout << input[i]<<" ";
+        cout << sortedInput[i]<<" ";
     }
     return 0;
 
